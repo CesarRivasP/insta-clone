00d19b5f3a7d384cfba4cd0673a37b1fdcc10776
CesarRivasP <cesarjose58@gmail.com>
2019-07-05 21:35:49 -0400

    Create InstaCLone Component

---
+++ src/insta-clone.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+class InstaClone extends Component {
+  render () {
+    return (
+      <View>
+        <Text>My Instagram Clone</Text>
+      </View>
+    );
+  }
+}
+
+export default InstaClone;
