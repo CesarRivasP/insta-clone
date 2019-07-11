91dc0ef3f8e300675cfd033ca8501f2792a9108b
cesarrivas10 <cesarjose58@gmail.com>
2019-07-10 22:10:09 -0400

    Added new styles and implemented a new section

--- src/insta-clone.js
+++ src/insta-clone.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
 class InstaClone extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      screenWidth: 0
+    }
+  }
+
+  componentDidMount(){
+    // alert(Dimensions.get('window').width)
+    this.setState({
+      screenWidth: Dimensions.get('window').width
+    })
+  }
+
   render () {
+    const { screenWidth } = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.tempNav}>
-          <Text style={styles.title}>InstaClone</Text>
+          <Text style={styles.titleApp}>InstaClone</Text>
         </View>
-        <View style={styles.userBar}>
 
+        <View style={styles.userBar}>
+          <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+            <Image
+              source={{ uri: 'https://buscatupsicologo.com/wp-content/uploads/2016/11/sidebar_usuario-corporativo.png' }}
+              style={styles.userPicture}
+            />
+            <Text style={styles.userTitle}>User</Text>
+          </View>
+          <View style={{ alignItems: 'center', paddingBottom: 15 }}>
+            <Text style={{ fontSize: 25 }}>...</Text>
+          </View>
         </View>
+
         <Image
-          style={styles.imageContainer}
+          // style={styles.picturePosted}
+          style={{ screenWidth, height: '50%' }}
           source={{ uri: "https://st.automobilemag.com/uploads/sites/11/2018/10/Singer-Vehicle-Design-Porsche-21.jpg"}}
         />
       </View>
@@ -27,9 +55,12 @@ const styles = StyleSheet.create({
     width: 100+'%',
     height: 100+'%'
   },
-  imageContainer: {
+  picturePosted: {
      width: 100+'%',
      height: 50+'%'
+  },
+  titleApp: {
+    fontSize: 20
   },
   tempNav: {
     width: 100+'%',
@@ -44,10 +75,19 @@ const styles = StyleSheet.create({
   userBar: {
     width: 100+'%',
     height: 50,
-    backgroundColor: 'rgb(250,250,250)'
+    backgroundColor: 'rgb(250,250,250)',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    justifyContent: 'space-between'
   },
-  title: {
-    fontSize: 20
+  userPicture: {
+    width: 40,
+    height: 40,
+    borderRadius: 20
+  },
+  userTitle: {
+    marginLeft: 10
   }
 })
 export default InstaClone;
