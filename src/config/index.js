26607bef74a3abbe193b924c337535e649993bf9
cesarrivas10 <cesarjose58@gmail.com>
2019-07-11 07:14:14 -0400

    Added styles for contants

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,8 @@ export default {
     heartIcon: require('../../assets/heart.png'),
     arrowIcon: require('../../assets/arrow.png'),
     chatIcon: require('../../assets/sms.png')
+  },
+  styleConstants: {
+    rowHeight: 50
   }
 }
