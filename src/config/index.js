6c337382c3627bc9ff3a19e0dbbece995b25c7e2
cesarrivas10 <cesarjose58@gmail.com>
2019-07-10 22:09:51 -0400

    Create config image routes file

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+export default {
+  images: {
+    heartIcon: require('../../assets/heart.png'),
+    arrowIcon: require('../../assets/arrow.png'),
+    chatIcon: require('../../assets/sms.png')
+  }
+}
